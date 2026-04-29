1ba8c80a72102d3e13cc7e1eebff525a3782e64d
Abi <reidabi6@gmail.com>
2026-04-29 15:46:46 +0100

    fgff

--- Trash collecting game/Assets/AbiScripts/RandomSpawingTrash.cs
+++ Trash collecting game/Assets/AbiScripts/RandomSpawingTrash.cs
@@ -10,25 +10,25 @@ public class RandomSpawingTrash : MonoBehaviour
     public List <GameObject> trashList; // public for the AI Nav Script 
 
     [Header("Timer settings")]
-    [SerializeField] private float spawnInterval = 1f;
-    private float timer; 
+    [SerializeField] private float spawnInterval;
+    private float _timer; 
 
     private void FixedUpdate()
     {
-        timer += Time.deltaTime; //creates a frame-rate independent timer
-        if (timer >= spawnInterval)  // if timer is less than spaw interval do span logic 
+        _timer += Time.deltaTime; //creates a frame-rate independent timer
+        if (_timer >= spawnInterval)  // if timer is less than spaw interval do span logic 
         {
-            timer = 0f; 
+            _timer = 0f; 
 
             if (currentTrash >= maxTrash) return; // stop spawing the objects if there is more than the max 
 
             //----------SPAWN LOGIC DO NOT TOUCH-----------------
             //Spawing Positions
-            int spawnPointX = Random.Range(0, 0); 
-            int spawnPointY = Random.Range(0, 30);
-            int spawnPointZ = Random.Range(0, 20);
+            int spawnPointX = Random.Range(50, 30); 
+            int spawnPointY = Random.Range(50, 30);
+            int spawnPointZ = Random.Range(50, 20);
 
-            //creates the spawnposition 
+            //creates the spawnposition randomly in the square 
             Vector3 spawnPosition = new Vector3(spawnPointZ, spawnPointX, spawnPointY);
             //spawns the objects 
             GameObject spawnedTrash = Instantiate(trashPrefab, spawnPosition, Quaternion.identity);
@@ -39,4 +39,5 @@ public class RandomSpawingTrash : MonoBehaviour
             currentTrash++; //add spawned trash to the current trash
         }
     }
+
 }
